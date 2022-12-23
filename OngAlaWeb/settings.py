942dc364b0ce6415b05431a4623522742e5a9f3b
Devlomarco089 <105669977+Devlomarco089@users.noreply.github.com>
2022-12-23 13:41:21 -0300

    Update settings.py

--- OngAlaWeb/settings.py
+++ OngAlaWeb/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/home/Devlomarco089/Devlomarco089.pythonanywhere.com/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static',]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/home/Devlomarco089/Devlomarco089.pythonanywhere.com/static'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/home/Devlomarco089/Devlomarco089.pythonanywhere.com/media/'
+MEDIA_ROOT = '/home/Devlomarco089/Devlomarco089.pythonanywhere.com/media'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
